32ad9b6589059fa51b2e2557d5eafa560775efcc
Ivan Trunaev <trunaev@gmail.com>
2022-09-04 03:45:10 +0500

    UPDATE: mir-control setup set cpu cores number to configs

---
+++ libs/Mir/Config.pm
@@ -0,0 +1,25 @@
+package Mir::Config;
+
+use 5.024;
+use JSON::XS;
+
+use constant CONFIG_PATH => 'mir.config';
+
+
+
+sub _get {
+	my $text;
+	
+	open my $fh, CONFIG_PATH;
+	$text = join '', <$fh>;
+	close $fh;
+	
+	return decode_json $text;
+}
+
+sub get {
+	state $config = _get;
+	return $config;
+}
+
+1; 
